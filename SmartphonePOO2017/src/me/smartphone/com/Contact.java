eaaf8b555d35fd5a2f2668c3dfc7f4c2c29d282b
Pierre Balet <Pierre Balet@Pierre>
2017-06-02 09:59:06 +0200

    Création de la classe contact

---
+++ SmartphonePOO2017/src/me/smartphone/com/Contact.java
@@ -0,0 +1,50 @@
+package me.smartphone.com;
+
+import java.util.UUID;
+
+public class Contact {
+
+	private UUID contactID ;
+	private String lastname ;
+	private String firstname ;
+	private String birthdate ;
+	
+	public Contact(String lastname, String firstname, String birthdate) {
+		this.contactID = UUID.randomUUID();
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.birthdate = birthdate;
+	}
+	
+	public UUID getcontactID() {
+		return contactID;
+	}
+	
+	public void setIdcontact(UUID contactID) {
+		this.contactID = contactID;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getBirthdate() {
+		return birthdate;
+	}
+	
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+}
